9d9eccfd27645dfba883bfc0260d1328c7ecc501
ashkan <ashkan16ma96@gmail.com>
2023-06-01 19:10:16 +0330

    react-select added to weatherPage

--- src/pages/weather/WeatherPage.tsx
+++ src/pages/weather/WeatherPage.tsx
@@ -1,17 +1,39 @@
 import { Box } from '@mui/material';
+import { useState } from 'react';
+import Select from 'react-select';
+import cities from "../../ir.json"
+import styles from "./WeatherPage.module.css"
+
+const options = cities.map((city) => {
+    return { value: { lat: city.lat, lng: city.lng }, label: city.city }
+});
 
 const WeatherPage = () => {
+    const [selectedOption, setSelectedOption] = useState(null)
+
+    const handleChange = (selectedOption: any) => {
+        setSelectedOption(selectedOption)
+    };
+
     return (
         <Box
             sx={{
                 display: 'flex',
-                justifyContent: 'center',
+                padding: "2rem",
                 alignItems: 'center',
                 gap: "1rem",
                 flexDirection: 'column',
                 minHeight: '100%',
             }}
-        >weather page</Box>
+        >
+            <Select
+                placeholder="Enter City"
+                className={styles.citySelecter}
+                value={selectedOption}
+                onChange={handleChange}
+                options={options}
+            />
+        </Box>
     );
 }
 
