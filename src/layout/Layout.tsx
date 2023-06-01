1ba6f49972d7cd2fe53eea70e90ead928eeb811e
ashkan <ashkan16ma96@gmail.com>
2023-06-01 15:38:10 +0330

    side bar links connected to pages

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -11,6 +11,12 @@ import PersonIcon from '@mui/icons-material/Person';
 import CloudIcon from '@mui/icons-material/Cloud';
 import { ChildsComponentsType } from "../types";
 import Header from "./Header";
+import { Link } from "react-router-dom";
+
+const linkStyles = {
+    color: "inherit",
+    textDecoration: "none"
+}
 
 const Layout: React.FC<ChildsComponentsType> = ({ children }) => {
     return (
@@ -21,38 +27,46 @@ const Layout: React.FC<ChildsComponentsType> = ({ children }) => {
                     <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
                         <nav aria-label="main mailbox folders">
                             <List>
-                                <ListItem disablePadding>
-                                    <ListItemButton>
-                                        <ListItemIcon>
-                                            <DashboardIcon />
-                                        </ListItemIcon>
-                                        <ListItemText primary="Dashboard" />
-                                    </ListItemButton>
-                                </ListItem>
-                                <ListItem disablePadding>
-                                    <ListItemButton>
-                                        <ListItemIcon>
-                                            <ListIcon />
-                                        </ListItemIcon>
-                                        <ListItemText primary="Todos" />
-                                    </ListItemButton>
-                                </ListItem>
-                                <ListItem disablePadding>
-                                    <ListItemButton>
-                                        <ListItemIcon>
-                                            <CloudIcon />
-                                        </ListItemIcon>
-                                        <ListItemText primary="Weather" />
-                                    </ListItemButton>
-                                </ListItem>
-                                <ListItem disablePadding>
-                                    <ListItemButton>
-                                        <ListItemIcon>
-                                            <PersonIcon />
-                                        </ListItemIcon>
-                                        <ListItemText primary="Profile" />
-                                    </ListItemButton>
-                                </ListItem>
+                                <Link style={linkStyles} to="/">
+                                    <ListItem disablePadding>
+                                        <ListItemButton>
+                                            <ListItemIcon>
+                                                <DashboardIcon />
+                                            </ListItemIcon>
+                                            <ListItemText primary="Dashboard" />
+                                        </ListItemButton>
+                                    </ListItem>
+                                </Link>
+                                <Link style={linkStyles} to="/todos">
+                                    <ListItem disablePadding>
+                                        <ListItemButton>
+                                            <ListItemIcon>
+                                                <ListIcon />
+                                            </ListItemIcon>
+                                            <ListItemText primary="Todos" />
+                                        </ListItemButton>
+                                    </ListItem>
+                                </Link>
+                                <Link style={linkStyles} to="weather">
+                                    <ListItem disablePadding>
+                                        <ListItemButton>
+                                            <ListItemIcon>
+                                                <CloudIcon />
+                                            </ListItemIcon>
+                                            <ListItemText primary="Weather" />
+                                        </ListItemButton>
+                                    </ListItem>
+                                </Link>
+                                <Link style={linkStyles} to="profile">
+                                    <ListItem disablePadding>
+                                        <ListItemButton>
+                                            <ListItemIcon>
+                                                <PersonIcon />
+                                            </ListItemIcon>
+                                            <ListItemText primary="Profile" />
+                                        </ListItemButton>
+                                    </ListItem>
+                                </Link>
                             </List>
                         </nav>
                     </Box>
