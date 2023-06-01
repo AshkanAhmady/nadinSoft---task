2c1da99686967303f8530e4eae7e069ec89d75b9
ashkan <ashkan16ma96@gmail.com>
2023-06-02 01:44:12 +0330

    todos components added

---
+++ src/components/todos/todoForm/TodoForm.tsx
@@ -0,0 +1,45 @@
+import styles from "./TodoForm.module.css";
+import React, { useEffect, useRef, useState } from "react";
+import Button from '@mui/material/Button';
+import { TodoFormComponentPropsType } from "../../../types";
+
+const TodoForm: React.FC<TodoFormComponentPropsType> = (props) => {
+    const [input, setInput] = useState(props.edit ? props.edit.text : "");
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        inputRef.current!.focus();
+    }, []);
+
+    const changeHandler = (e: React.FormEvent<HTMLInputElement>) => {
+        setInput(e.currentTarget.value);
+    };
+
+    const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!input) {
+            alert("Please Fill The Input");
+            return;
+        }
+        props.submitTodo(input);
+        setInput("");
+    };
+
+    return (
+        <form
+            className={`${styles.todoForm} ${props.edit && styles.editForm}`}
+            onSubmit={submitHandler}
+        >
+            <input
+                type="text"
+                onChange={changeHandler}
+                value={input}
+                placeholder={props.edit ? "Update Todo ..." : "Add Todo ..."}
+                ref={inputRef}
+            />
+            <Button type="submit">{props.edit ? "Update" : "Add"}</Button>
+        </form>
+    );
+};
+
+export default TodoForm;
--- src/pages/todos/TodosPage.tsx
+++ src/pages/todos/TodosPage.tsx
@@ -1,6 +1,48 @@
 import { Box } from '@mui/material';
+import { useState } from "react";
+import TodoForm from '../../components/todos/todoForm/TodoForm';
+import TodoList from '../../components/todos/todoList/TodoList';
+import { TodoType } from '../../types';
 
 const TodosPage = () => {
+    const [todos, setTodos] = useState<any[]>([]);
+
+    const submitTodo = (input: string) => {
+        const newTodo = {
+            id: Math.floor(Math.random() * 1000),
+            text: input,
+            isComplete: false,
+        };
+        setTodos([...todos, newTodo]);
+    };
+
+    const completeTodo = (id: number) => {
+        const cloneTodos: TodoType[] = [...todos];
+        const index = cloneTodos.findIndex((todo) => todo.id === id);
+        const todo = { ...cloneTodos[index] };
+
+        todo.isComplete = !todo.isComplete;
+        cloneTodos[index] = todo;
+
+        setTodos(cloneTodos);
+    };
+
+    const deleteTodo = (id: number) => {
+        const updatedTodos = todos.filter((todo) => todo.id !== id);
+        setTodos(updatedTodos);
+    };
+
+    const updateTodo = (newValue: string, id: number) => {
+        const cloneTodos: TodoType[] = [...todos];
+        const index = cloneTodos.findIndex((todo) => todo.id === id);
+        const todo = { ...cloneTodos[index] };
+
+        todo.text = newValue;
+        cloneTodos[index] = todo;
+
+        setTodos(cloneTodos);
+    };
+
     return (
         <Box
             sx={{
@@ -11,8 +53,21 @@ const TodosPage = () => {
                 flexDirection: 'column',
                 minHeight: '100%',
             }}
-        >todos page</Box>
+        >
+            <TodoForm submitTodo={submitTodo} />
+            <TodoList
+                onComplete={completeTodo}
+                onDelete={deleteTodo}
+                todos={todos}
+                updateTodo={updateTodo}
+            />
+        </Box>
     );
 }
 
 export default TodosPage;
+
+
+
+
+
